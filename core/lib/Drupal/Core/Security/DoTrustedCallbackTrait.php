4843cd3b30ef5e7c25ad75fe26860d8ca5bceaa1
poojaganjage <pooja260895@gmail.com>
2020-10-12 23:10:14 +0530

    DoTrustedCallbackTrait.php file updated

--- core/lib/Drupal/Core/Security/DoTrustedCallbackTrait.php
+++ core/lib/Drupal/Core/Security/DoTrustedCallbackTrait.php
@@ -96,8 +96,8 @@ trait DoTrustedCallbackTrait {
         throw new UntrustedCallbackException($message);
       }
     }
-
-    return call_user_func_array($callback, $args);
+    // @TODO Allow named arguments in https://www.drupal.org/node/3174150
+    return call_user_func_array($callback, array_values($args));
   }
 
 }
